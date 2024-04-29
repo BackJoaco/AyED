eea555e85247475bc470964d5733c0b8f14e62e2
BackJoaco <joaquingaricano@gmail.com>
2024-04-29 20:00:37 -0300

    practica 3 comenzada

---
+++ Practicas/Practica 3/src/ejercicio2/RecorridosAG.java
@@ -0,0 +1,47 @@
+package ejercicio2;
+import java.util.*;
+
+import ejercicio1.*;
+
+public class RecorridosAG {
+	private List<Integer> lista;
+	public static boolean esImpar(int n) {
+		return n % 2  !=0;
+	}
+	
+	public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree <Integer> a, Integer n){
+		int dato = a.getData();
+		if ((dato % 2 != 0) && (dato > n)) {
+			lista.add(dato);
+;		}
+		if (a.hasChildren()) { // si el nodo tiene hijos
+			List<GeneralTree<Integer>> l = a.getChildren(); // obtenemos la lista de hijos
+			for (GeneralTree<Integer> hijo : l) { // iteramos sobre esa lista 
+				numerosImparesMayoresQuePreOrden(hijo, n); // los procesamos recursivamente
+			}
+		}
+		return lista;
+	}
+	
+	private static void  InOrden (List<Integer> lista,GeneralTree<Integer> a, Integer n) {
+		if (!a.isEmpty()) {
+			int i = 0;
+			for (GeneralTree<Integer> child:a.getChildren()) {
+				i++;
+				InOrden(lista, child, n);
+				if ((i==1) && (a.getData()>n) && (esImpar(a.getData()))) {
+					lista.add(a.getData());
+				}
+			}
+			if ((i==0) && (a.getData()>n) && (esImpar(a.getData()))) {
+				lista.add(a.getData());
+			}
+		}
+	}
+	public List<Integer> numerosImparesMayoresQueInOrden (GeneralTree <Integer> a, Integer n){
+		List<Integer> list = new LinkedList<Integer>();
+		InOrden(list, a, n);
+		return list;
+	}
+
+}
